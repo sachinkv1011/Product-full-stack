5f2fb33a7250b14f260314139cff703aebc2d206
SACHIN K V <sachin.kv.1011@gmail.com>
2022-12-07 15:40:11 +0530

    product add page

---
+++ src/app/add-product/add-product.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+export class AddProductComponent {
+  productCode=""
+  productName=""
+  manufactureDate=""
+  expDate=""
+  brand=""
+  price=""
+  sellerName=""
+  distributorName=""
+
+}
